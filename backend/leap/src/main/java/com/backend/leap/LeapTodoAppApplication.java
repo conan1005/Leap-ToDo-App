7eec11ae7fb765536947b8e7da48c9e8a8b24a47
Aaditya Gaur <aadifab2001@gmail.com>
2024-01-20 14:58:15 +0530

    First Commit

---
+++ backend/leap/src/main/java/com/backend/leap/LeapTodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.backend.leap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapTodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeapTodoAppApplication.class, args);
+	}
+
+}
